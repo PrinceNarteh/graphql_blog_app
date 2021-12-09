41274f992d46dbe007a05f52b01190beecfe6c70
Prince Narteh <prinarteh@gmail.com>
2021-12-09 19:42:15 +0000

    add me query to get authenticated user info

--- src/resolvers/Queries/Query.ts
+++ src/resolvers/Queries/Query.ts
@@ -1,6 +1,12 @@
 import { Context } from "../../server";
 
 export const RootQuery = {
+  me: (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null;
+    return prisma.user.findUnique({
+      where: { id: userInfo.userId },
+    });
+  },
   posts: (_: any, __: any, { prisma }: Context) => {
     return prisma.post.findMany({
       orderBy: [{ createdAt: "desc" }],
